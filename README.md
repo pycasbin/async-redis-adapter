7454236ea7cc342fa3a4e7aee75ca270eeaa573b
Haozhe Pan <bust.dev@outlook.com>
2023-09-17 13:20:31 +0800

    feat: complete async_redis_adapter (#2)

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# async-redis-adapter
+Async Redis Adapter for PyCasbin
+====
+
+[![GitHub Actions](https://github.com/pycasbin/async-redis-adapter/workflows/build/badge.svg?branch=master)](https://github.com/pycasbin/async-redis-adapter/actions)
+[![Coverage Status](https://coveralls.io/repos/github/pycasbin/async-redis-adapter/badge.svg?branch=master)](https://coveralls.io/github/pycasbin/async-redis-adapter?branch=master)
+[![Version](https://img.shields.io/pypi/v/casbin_async_redis_adapter.svg)](https://pypi.org/project/casbin_async_redis_adapter/)
+[![PyPI - Wheel](https://img.shields.io/pypi/wheel/casbin_async_redis_adapter.svg)](https://pypi.org/project/casbin_async_redis_adapter/)
+[![Pyversions](https://img.shields.io/pypi/pyversions/casbin_async_redis_adapter.svg)](https://pypi.org/project/casbin_async_redis_adapter/)
+[![Download](https://img.shields.io/pypi/dm/casbin_async_redis_adapter.svg)](https://pypi.org/project/casbin_async_redis_adapter/)
+[![License](https://img.shields.io/pypi/l/casbin_async_redis_adapter.svg)](https://pypi.org/project/casbin_async_redis_adapter/)
+
+Async Redis Adapter is the async [redis](https://redis.io/) adapter for [PyCasbin](https://github.com/casbin/pycasbin).
+With this
+library, Casbin can load policy from redis or save policy to it.
+
+## Installation
+
+```
+pip install casbin_async_redis_adapter
+```
+
+## Simple Example
+
+```python
+import asyncio
+from casbin_async_redis_adapter import Adapter
+import casbin
+
+
+async def get_enforcer():
+    adapter = Adapter("localhost", 6379, encoding="utf-8")
+    e = casbin.AsyncEnforcer("rbac_model.conf", adapter)
+    model = e.get_model()
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["alice", "data1", "read"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["bob", "data2", "write"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["data2_admin", "data2", "read"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["data2_admin", "data2", "write"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("g", "g", ["alice", "data2_admin"])
+    await adapter.save_policy(model)
+
+    e = casbin.AsyncEnforcer("rbac_model.conf", adapter)
+    await e.load_policy()
+
+    return e
+
+
+sub = "alice"  # the user that wants to access a resource.
+obj = "data1"  # the resource that is going to be accessed.
+act = "read"  # the operation that the user performs on the resource.
+
+
+async def main():
+    e = await get_enforcer()
+    if e.enforce("alice", "data1", "read"):
+        print("alice can read data1")
+    else:
+        print("alice can not read data1")
+
+
+asyncio.run(main())
+```
+
+## Configuration
+
+`Adapter()` enable decode_responses by default and supports any Redis parameter configuration.
+
+To use casbin_redis_adapter, you must provide the following parameter configuration
+
+- `host`: address of the redis service
+- `port`: redis service port
+
+The following parameters are provided by default
+
+- `db`: redis database, default is `0`
+- `username`: redis username, default is `None`
+- `password`: redis password, default is `None`
+- `key`: casbin rule to store key, default is `casbin_rules`
+
+For more parameters, please follow [redis-py](https://redis.readthedocs.io/en/stable/connections.html#redis.Redis)
+
+### Getting Help
+
+- [PyCasbin](https://github.com/casbin/pycasbin)
+
+### License
+
+This project is licensed under the [Apache 2.0 license](LICENSE).
---
+++ tests/test_adapter.py
@@ -0,0 +1,237 @@
+from casbin_async_redis_adapter.adapter import Adapter, CasbinRule
+
+from unittest import IsolatedAsyncioTestCase
+import redis
+import casbin
+import os
+
+
+def get_fixture(path):
+    """
+    get model path
+    """
+    dir_path = os.path.split(os.path.realpath(__file__))[0] + "/"
+    return os.path.abspath(dir_path + path)
+
+
+async def get_enforcer():
+    adapter = Adapter("localhost", 6379, encoding="utf-8")
+    e = casbin.AsyncEnforcer(get_fixture("rbac_model.conf"), adapter)
+    model = e.get_model()
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["alice", "data1", "read"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["bob", "data2", "write"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["data2_admin", "data2", "read"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("p", "p", ["data2_admin", "data2", "write"])
+    await adapter.save_policy(model)
+
+    model.clear_policy()
+    model.add_policy("g", "g", ["alice", "data2_admin"])
+    await adapter.save_policy(model)
+
+    e = casbin.AsyncEnforcer(get_fixture("rbac_model.conf"), adapter)
+    await e.load_policy()
+
+    return e
+
+
+def clear_db(dbname):
+    client = redis.Redis()
+    client.delete(dbname)
+
+
+class TestConfig(IsolatedAsyncioTestCase):
+    """
+    unittest
+    """
+
+    def setUp(self):
+        clear_db("casbin_rules")
+
+    def tearDown(self):
+        clear_db("casbin_rules")
+
+    async def test_enforcer_basic(self):
+        """
+        test policy
+        """
+        e = await get_enforcer()
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertTrue(e.enforce("alice", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+    async def test_add_policy(self):
+        """
+        test add_policy
+        """
+        e = await get_enforcer()
+        adapter = e.get_adapter()
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertTrue(e.enforce("alice", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+        # test add_policy after insert 2 rules
+        await adapter.add_policy(sec="p", ptype="p", rule=("alice", "data1", "write"))
+        await adapter.add_policy(sec="p", ptype="p", rule=("bob", "data2", "read"))
+
+        # reload policies from database
+        await e.load_policy()
+
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertTrue(e.enforce("alice", "data1", "write"))
+        self.assertTrue(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertTrue(e.enforce("alice", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+    async def test_remove_policy(self):
+        """
+        test remove_policy
+        """
+        e = await get_enforcer()
+        adapter = e.get_adapter()
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertTrue(e.enforce("alice", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+        # test remove_policy after delete a role definition
+        result = await adapter.remove_policy(
+            sec="g", ptype="g", rule=("alice", "data2_admin")
+        )
+
+        # reload policies from database
+        await e.load_policy()
+
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertFalse(e.enforce("alice", "data2", "read"))
+        self.assertFalse(e.enforce("alice", "data2", "write"))
+        self.assertTrue(result)
+
+    async def test_remove_policy_no_remove_when_rule_is_incomplete(self):
+        adapter = Adapter("localhost", 6379)
+        e = casbin.AsyncEnforcer(get_fixture("rbac_with_resources_roles.conf"), adapter)
+
+        await adapter.add_policy(sec="p", ptype="p", rule=("alice", "data1", "write"))
+        await adapter.add_policy(sec="p", ptype="p", rule=("alice", "data1", "read"))
+        await adapter.add_policy(sec="p", ptype="p", rule=("bob", "data2", "read"))
+        await adapter.add_policy(
+            sec="p", ptype="p", rule=("data_group_admin", "data_group", "write")
+        )
+        await adapter.add_policy(sec="g", ptype="g", rule=("alice", "data_group_admin"))
+        await adapter.add_policy(sec="g", ptype="g2", rule=("data2", "data_group"))
+
+        await e.load_policy()
+
+        self.assertTrue(e.enforce("alice", "data1", "write"))
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+        # test remove_policy doesn't remove when given an incomplete policy
+        await adapter.remove_policy(sec="p", ptype="p", rule=("alice", "data1"))
+        await e.load_policy()
+
+        self.assertTrue(e.enforce("alice", "data1", "write"))
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+    async def test_save_policy(self):
+        """
+        test save_policy
+        """
+
+        e = await get_enforcer()
+        self.assertFalse(e.enforce("alice", "data4", "read"))
+
+        model = e.get_model()
+        model.clear_policy()
+
+        model.add_policy("p", "p", ("alice", "data4", "read"))
+
+        adapter = e.get_adapter()
+        await adapter.save_policy(model)
+
+        self.assertTrue(e.enforce("alice", "data4", "read"))
+
+    async def test_remove_filtered_policy(self):
+        """
+        test remove_filtered_policy
+        """
+        e = await get_enforcer()
+        adapter = e.get_adapter()
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertTrue(e.enforce("alice", "data2", "read"))
+        self.assertTrue(e.enforce("alice", "data2", "write"))
+
+        result = await adapter.remove_filtered_policy(
+            "g", "g", 6, "alice", "data2_admin"
+        )
+        await e.load_policy()
+        self.assertFalse(result)
+
+        result = await adapter.remove_filtered_policy(
+            "g", "g", 0, *[f"v{i}" for i in range(7)]
+        )
+        await e.load_policy()
+        self.assertFalse(result)
+
+        result = await adapter.remove_filtered_policy(
+            "g", "g", 0, "alice", "data2_admin"
+        )
+        await e.load_policy()
+        self.assertTrue(result)
+        self.assertTrue(e.enforce("alice", "data1", "read"))
+        self.assertFalse(e.enforce("alice", "data1", "write"))
+        self.assertFalse(e.enforce("bob", "data2", "read"))
+        self.assertTrue(e.enforce("bob", "data2", "write"))
+        self.assertFalse(e.enforce("alice", "data2", "read"))
+        self.assertFalse(e.enforce("alice", "data2", "write"))
+
+    def test_str(self):
+        """
+        test __str__ function
+        """
+        rule = CasbinRule(ptype="p", v0="alice", v1="data1", v2="read")
+        self.assertEqual(rule.__str__(), "p, alice, data1, read")
+
+    def test_dict(self):
+        """
+        test dict function
+        """
+        rule = CasbinRule(ptype="p", v0="alice", v1="data1", v2="read")
+        self.assertEqual(
+            rule.dict(), {"ptype": "p", "v0": "alice", "v1": "data1", "v2": "read"}
+        )
+
+    def test_repr(self):
+        """
+        test __repr__ function
+        """
+        rule = CasbinRule(ptype="p", v0="alice", v1="data1", v2="read")
+        self.assertEqual(repr(rule), '<CasbinRule :"p, alice, data1, read">')
